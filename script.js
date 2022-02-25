a78c7fce53c3d532b67ca639479b8f2e9f3ff359
Justin Pascual <chrono.social@outlook.com>
2022-02-25 14:51:18 +0800

    ⚡️ Code goal completed

---
+++ script.js
@@ -0,0 +1,21 @@
+const setProperty = (index, color) => {
+  return document.documentElement.style.setProperty(`--color-calendar-graph-day-L${index}-bg`, color);;
+}
+
+
+let chosen_color = '#ff66c7';
+let color_L1 = `${chosen_color}50`
+let color_L2 = `${chosen_color}88`
+let color_L3 = `${chosen_color}99`
+
+for (let index = 1; index <= 4; index++) {
+  if(index == 1) {
+    setProperty(index, color_L1)
+  } else if (index == 2) {
+    setProperty(index,  color_L2)
+  } else if (index == 3) {
+    setProperty(index,  color_L3)
+  } else if (index == 4) {
+    setProperty(index, chosen_color)
+  }
+}
