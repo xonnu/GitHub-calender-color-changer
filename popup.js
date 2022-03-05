0a7557901194ece9e26bda35882f158e86a004de
Justin Pascual <chrono.social@outlook.com>
2022-03-05 21:06:03 +0800

    💄 adding style and color picker

--- popup.js
+++ popup.js
@@ -1,8 +1,17 @@
 window.addEventListener('load', () => {
   const set_button = document.querySelector('#set-color-btn')
   const set_input = document.querySelector('#set-color-input')
+  const set_input_picker = document.querySelector('#set-color-input-picker')
 
   if (set_button != null && set_input != null) {
+    set_input_picker.addEventListener('change', () => {
+      chrome.storage.sync.set({
+        "github_calendar_color": set_input_picker.value
+      }, () => {
+        // success message
+      })
+    })
+
     set_button.addEventListener('click', () => {
       chrome.storage.sync.set({
         "github_calendar_color": set_input.value
